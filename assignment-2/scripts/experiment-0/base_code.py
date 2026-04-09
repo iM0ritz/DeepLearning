4101cdb2876aadb6cdc0f602819dad56561fb950
benbekir <ben.bekir.ertugrul@gmail.com>
2026-04-09 20:17:08 +0200

    copy working code from florians file

--- assignment-2/scripts/experiment-0/base_code.py
+++ assignment-2/scripts/experiment-0/base_code.py
@@ -188,71 +188,111 @@ class PositionalEmbedding(layers.Layer):
         return config
 
 
+import keras
+from keras import layers, ops
+
 class TransformerDecoder(layers.Layer):
     def __init__(self, embed_dim, latent_dim, num_heads, **kwargs):
         super().__init__(**kwargs)
         self.embed_dim = embed_dim
         self.latent_dim = latent_dim
         self.num_heads = num_heads
+        self.supports_masking = True
+
         self.attention_1 = layers.MultiHeadAttention(
-            num_heads=num_heads, key_dim=embed_dim
+            num_heads=num_heads,
+            key_dim=embed_dim,
+            name="self_attention",
         )
         self.attention_2 = layers.MultiHeadAttention(
-            num_heads=num_heads, key_dim=embed_dim
+            num_heads=num_heads,
+            key_dim=embed_dim,
+            name="cross_attention",
         )
         self.dense_proj = keras.Sequential(
             [
                 layers.Dense(latent_dim, activation="relu"),
                 layers.Dense(embed_dim),
-            ]
+            ],
+            name="dense_proj",
         )
         self.layernorm_1 = layers.LayerNormalization()
         self.layernorm_2 = layers.LayerNormalization()
         self.layernorm_3 = layers.LayerNormalization()
-        self.supports_masking = True
+
+    def build(self, input_shape):
+        # input_shape should be [decoder_shape, encoder_shape]
+        decoder_shape = input_shape[0]
+        encoder_shape = input_shape[1]
+
+        self.attention_1.build(decoder_shape, decoder_shape, decoder_shape)
+        self.attention_2.build(decoder_shape, encoder_shape, encoder_shape)
+        self.dense_proj.build(decoder_shape)
+        self.layernorm_1.build(decoder_shape)
+        self.layernorm_2.build(decoder_shape)
+        self.layernorm_3.build(decoder_shape)
+
+        super().build(input_shape)
 
     def call(self, inputs, mask=None):
-        inputs, encoder_outputs = inputs
-        causal_mask = self.get_causal_attention_mask(inputs)
+        decoder_inputs = inputs[0]
+        encoder_outputs = inputs[1]
 
-        if mask is None:
-            inputs_padding_mask, encoder_outputs_padding_mask = None, None
-        else:
-            inputs_padding_mask, encoder_outputs_padding_mask = mask
+        causal_mask = self.get_causal_attention_mask(decoder_inputs)
 
-        attention_output_1 = self.attention_1(
-            query=inputs,
-            value=inputs,
-            key=inputs,
+        decoder_padding_mask = None
+        encoder_padding_mask = None
+
+        if mask is not None:
+            if isinstance(mask, (list, tuple)):
+                if len(mask) > 0:
+                    decoder_padding_mask = mask[0]
+                if len(mask) > 1:
+                    encoder_padding_mask = mask[1]
+            else:
+                decoder_padding_mask = mask
+
+        attn_output_1 = self.attention_1(
+            query=decoder_inputs,
+            value=decoder_inputs,
+            key=decoder_inputs,
             attention_mask=causal_mask,
-            query_mask=inputs_padding_mask,
+            query_mask=decoder_padding_mask,
+            value_mask=decoder_padding_mask,
+            key_mask=decoder_padding_mask,
         )
-        out_1 = self.layernorm_1(inputs + attention_output_1)
+        out_1 = self.layernorm_1(decoder_inputs + attn_output_1)
 
-        attention_output_2 = self.attention_2(
+        attn_output_2 = self.attention_2(
             query=out_1,
             value=encoder_outputs,
             key=encoder_outputs,
-            query_mask=inputs_padding_mask,
-            key_mask=encoder_outputs_padding_mask,
+            query_mask=decoder_padding_mask,
+            value_mask=encoder_padding_mask,
+            key_mask=encoder_padding_mask,
         )
-        out_2 = self.layernorm_2(out_1 + attention_output_2)
+        out_2 = self.layernorm_2(out_1 + attn_output_2)
 
         proj_output = self.dense_proj(out_2)
         return self.layernorm_3(out_2 + proj_output)
 
     def get_causal_attention_mask(self, inputs):
-        input_shape = ops.shape(inputs)
-        batch_size, sequence_length = input_shape[0], input_shape[1]
-        i = ops.arange(sequence_length)[:, None]
-        j = ops.arange(sequence_length)
-        mask = ops.cast(i >= j, dtype="int32")
-        mask = ops.reshape(mask, (1, input_shape[1], input_shape[1]))
-        mult = ops.concatenate(
-            [ops.expand_dims(batch_size, -1), ops.convert_to_tensor([1, 1])],
-            axis=0,
-        )
-        return ops.tile(mask, mult)
+        import tensorflow as tf
+
+        input_shape = tf.shape(inputs)
+        batch_size = input_shape[0]
+        seq_len = input_shape[1]
+
+        i = tf.range(seq_len)[:, None]
+        j = tf.range(seq_len)[None, :]
+        mask = tf.cast(i >= j, dtype=tf.bool)
+
+        mask = tf.expand_dims(mask, axis=0)
+        mask = tf.broadcast_to(mask, [batch_size, seq_len, seq_len])
+        return mask
+
+    def compute_output_shape(self, input_shape):
+        return input_shape[0]
 
     def get_config(self):
         config = super().get_config()
@@ -272,19 +312,26 @@ num_heads = 8
 encoder_inputs = keras.Input(shape=(None,), dtype="int64", name="encoder_inputs")
 x = PositionalEmbedding(sequence_length, vocab_size, embed_dim)(encoder_inputs)
 encoder_outputs = TransformerEncoder(embed_dim, latent_dim, num_heads)(x)
-encoder = keras.Model(encoder_inputs, encoder_outputs)
 
 decoder_inputs = keras.Input(shape=(None,), dtype="int64", name="decoder_inputs")
 encoded_seq_inputs = keras.Input(shape=(None, embed_dim), name="decoder_state_inputs")
+
 x = PositionalEmbedding(sequence_length, vocab_size, embed_dim)(decoder_inputs)
-x = TransformerDecoder(embed_dim, latent_dim, num_heads)([x, encoder_outputs])
+x = TransformerDecoder(embed_dim, latent_dim, num_heads)([x, encoded_seq_inputs])
 x = layers.Dropout(0.5)(x)
 decoder_outputs = layers.Dense(vocab_size, activation="softmax")(x)
-decoder = keras.Model([decoder_inputs, encoded_seq_inputs], decoder_outputs)
+
+decoder = keras.Model(
+    [decoder_inputs, encoded_seq_inputs],
+    decoder_outputs,
+    name="decoder",
+)
+
+final_decoder_outputs = decoder([decoder_inputs, encoder_outputs])
 
 transformer = keras.Model(
     {"encoder_inputs": encoder_inputs, "decoder_inputs": decoder_inputs},
-    decoder_outputs,
+    final_decoder_outputs,
     name="transformer",
 )
 
